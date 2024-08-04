eee674fb66e528398172469e0b5da4341410063d
jakiyasafiya123 <jakiyapeer@gmail.com>
2024-08-04 18:46:04 +0530

    statement updated

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-console.log("welcome to day 2")
-alert()
+/*console.log("welcome to day 2")
+alert("click ok enter into yhe website")*/
+/*let result = prompt("Enter your Name")
+ 
+console.log(result)*/
+/*let result1= confirm("Are you sure to enter the webiste")
+console.log(result1);*/
+var age=prompt("what is your age",18);
+
+if(age<18){
+    alert('you are allowed to votting');
+}
+else {
+    alert('you are not allowed to votting ');
+}
